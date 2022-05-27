ad3224939a3ea8e3c80fe3844f0adb566365f1b1
Evgeny Kucherenko <evg.kch@gmail.com>
2022-05-27 14:37:21 +0300

    feature: d.ts added to the dist fold

---
+++ dist/index.d.ts
@@ -0,0 +1,87 @@
+/**
+ * A lib to create a channel to send and receive messages
+ */
+export declare type Message = string | number | symbol;
+/**
+ * Match signal with its args
+ */
+export declare type MessageMap = {
+    [msg in Message]: any[];
+};
+export declare type Subscribers<M extends MessageMap> = Map<keyof M, Set<(...args: M[keyof M]) => any>>;
+export default class Channel<M extends MessageMap> {
+    #private;
+    readonly tx: Tx<M>;
+    readonly rx: Rx<M>;
+    /**
+     * Getting all signals
+     */
+    get messages(): (keyof M)[];
+    /**
+     * Clear all subsribers
+     */
+    clear(): void;
+}
+/**
+ * Message Transmitter
+ */
+export declare class Tx<M extends MessageMap> {
+    #private;
+    constructor(subscribers: Subscribers<M>);
+    /**
+     * Emit a signal that provides to the signal's subscribers.
+     *
+     * Ex.: tx.send('msg', [...args])
+     * Returns true if the signal had listeners, false otherwise
+     */
+    send<S extends keyof M>(msg: S, ...args: M[S]): boolean;
+    /**
+     * Emit an async signal that provides to the signal's subscribers.
+     *
+     * Ex.: tx.send_async('msg', [...args])
+     * Returns Promise<true> if the event had listeners, Promise<false> otherwise
+     */
+    send_async<S extends keyof M>(signal: S, ...args: M[S]): Promise<boolean>;
+}
+/**
+ * Message Receiver
+ */
+export declare class Rx<M extends MessageMap> {
+    #private;
+    constructor(subscribers: Subscribers<M>);
+    /**
+     * Subscribe on a message.
+     * Returns the provided listener.
+     *
+     * Ex.: rx.on('msg', listener)
+     */
+    on<S extends keyof M>(msg: S, listener: (...args: M[S]) => any): (...args: M[S]) => any;
+    /**
+     * Subscribe on a message once (subscriber will be deleted after send).
+     * Returns the provided listener.
+     *
+     * Ex.: rx.once('msg', listener)
+     */
+    once<S extends keyof M>(msg: S, listener: (...args: M[S]) => any): (...args: M[S]) => any;
+    /**
+     * Subscribe on a signal weak (subscriber will be deleted if it will be dead).
+     * Returns a WeakRef of the provided listener.
+     *
+     * Ex.: rx.onweak('msg', listener)
+     */
+    onweak<S extends keyof M>(msg: S, listener: (...args: M[S]) => any): WeakRef<(...args: M[S]) => any>;
+    /**
+     * Ubsubscribe listener from the message.
+     * Returns true if message and listener existed, false otherwise.
+     *
+     * Ex.: rx.off('msg', listener)
+     */
+    off<S extends keyof M>(msg: S, listener: (...args: M[S]) => any): boolean;
+    /**
+     * Ubsubscribe all listeners from the message.
+     * Returns true if message existed, false otherwise.
+     *
+     * Ex.: rx.off_all('msg')
+     */
+    off_all(msg: keyof M): boolean;
+}
